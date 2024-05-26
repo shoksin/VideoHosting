cad7879a0274dba5f8702aa492839d8168cab15e
Artsiom Naumchyk <anaumcik300@gmail.com>
2024-05-26 19:04:54 +0300

    initial commit

---
+++ server.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"ginTutorial/controller"
+	"ginTutorial/middlewares"
+	"ginTutorial/service"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	gindump "github.com/tpkeeper/gin-dump"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	VideoController controller.VideoController = controller.New(videoService)
+)
+
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
+func main() {
+	setupLogOutput()
+
+	server := gin.New()
+
+	server.Static("/css", "./templates/css")
+
+	server.LoadHTMLGlob("templates/*.html")
+
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
+
+	apiRoutes := server.Group("/api")
+	{
+		apiRoutes.GET("/videos", func(ctx *gin.Context) {
+			ctx.JSON(200, VideoController.FindAll())
+		})
+
+		apiRoutes.POST("/videos", func(ctx *gin.Context) {
+			err := VideoController.Save(ctx)
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			} else {
+				ctx.JSON(http.StatusOK, gin.H{"message": "Video Input is Valid!"})
+			}
+		})
+
+	}
+
+	viewRoutes := server.Group("/view")
+	{
+		viewRoutes.GET("/videos", VideoController.ShowAll)
+	}
+
+	server.Run(":8080")
+}
